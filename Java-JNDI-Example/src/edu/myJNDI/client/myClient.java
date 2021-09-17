7741176dbbc07c6a71409464a5b819894f007123
chenlvtang <1821762414@qq.com>
2021-09-17 10:16:50 +0800

    LDAP

--- Java-JNDI-Example/src/edu/myJNDI/client/myClient.java
+++ Java-JNDI-Example/src/edu/myJNDI/client/myClient.java
@@ -12,8 +12,9 @@ import java.rmi.RemoteException;
 public class myClient {
     public static void main(String[] args) throws MalformedURLException, NotBoundException, RemoteException {
         try {
-            //String url = "rmi://127.0.0.1:1099/myRMI";
-            String url = "rmi://127.0.0.1:1099/test"; //for hacker
+            //String url = "rmi://127.0.0.1:1099/myRMI"; //for user
+            //String url = "rmi://127.0.0.1:1099/test"; //for hacker_RMI
+            String url ="ldap://127.0.0.1:1389/xxx"; //for hacker_LDAP
             Context ctx = new InitialContext();
             RemoteInterface myRemote = (RemoteInterface)ctx.lookup(url);
             String result = myRemote.sayHello("chenlvtang");
