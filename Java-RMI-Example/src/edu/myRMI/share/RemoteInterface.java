ebb06860d5f8e43db6bd6670bcadd0ef94d0e5e4
chenlvtang <1821762414@qq.com>
2021-12-06 11:45:36 +0800

    fix

--- Java-RMI-Example/src/edu/myRMI/share/RemoteInterface.java
+++
@@ -1,8 +0,0 @@
-package edu.myRMI.share;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemoteInterface extends Remote {
-    String sayHello(String name) throws RemoteException;
-}
--- Java-RMI-Exp_CC3/src/main/java/shared/RemoteInter.java
+++
@@ -1,8 +0,0 @@
-package shared;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RemoteInter extends Remote {
-    String sayHello(String name) throws RemoteException;
-}
